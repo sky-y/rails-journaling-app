771a86aa57e2e2721014162bb4d96fd9785dd4a5
Yuki Fujiwara <sky.y.0079@gmail.com>
2021-12-17 15:15:51 +0900

    [add] Subjectモデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_12_17_061308) do
+
+  create_table "subjects", charset: "utf8mb4", force: :cascade do |t|
+    t.string "odai", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
